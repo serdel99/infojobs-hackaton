548098122cb525bab978f077529e25b8bb3fbcf8
Sergio Delgado <sergio.e.delgado99@gmail.com>
2023-05-28 16:03:42 -0500

    fix: import

--- src/app/infojobs-callback-uri/route.js
+++ src/app/infojobs-callback-uri/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server"
-import { getLinkedinProfile } from "../../../linkedin-scrapper"
+
 
 export async function GET(request) {
     // Todo Manage Infojobs Authentication
